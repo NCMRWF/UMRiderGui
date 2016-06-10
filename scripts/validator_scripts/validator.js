ed1e3670c77af721552241b0e43d2c1216a2f31c
arditecht <rickdasgupta9@gmail.com>
2016-06-10 12:53:11 +0530

    stable scripts

--- scripts/validator_scripts/validator.js
+++ scripts/validator_scripts/validator.js
@@ -1,6 +1,11 @@
-function is_number(obj){
-  if (is_number(obj.value)){
-    return true;
-  }
-  return false;
+function descriptor(obj){
+  var getter = obj.innerHTML;
+  var ID = (getter.slice(0, -2)).trim();
+
+  var ele = document.getElementById(ID);
+  document.getElementById("descript").innerHTML = ele.value;
+}
+
+function clear_descript(){
+  document.getElementById("descript").innerHTML = '';
 }
