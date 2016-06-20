dcebaf491dc65d1c10228570e9c0aa17f4421b49
arditecht <rickdasgupta9@gmail.com>
2016-06-20 12:07:53 +0530

    major changes.
    added interface to select var name, stash pairs. Added description
    handler to all of them

--- scripts/form_processor.php
+++ scripts/form_processor.php
@@ -33,6 +33,98 @@ echo "SUCCESS: File has been created";
 fclose($cfgfile);
 */
 
+$FORM_CSS     = 'styler.css';
+
+$dom = new DOMDocument("2.0");
+//=========================================================================//
+//styling constants
+$HEADER_FONT_LINK =  'https://fonts.googleapis.com/css?family=Titillium+Web:300';
+$OB_LABEL_FONT_LINK = 'https://fonts.googleapis.com/css?family=Oswald';
+$LABEL_FONT_LINK = 'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300';
+$SUBMIT_FONT_LINK = 'https://fonts.googleapis.com/css?family=Oswald:700';
+//load the xml file to read the object's format from.
+//=========================================================================//
+$link = $dom->createElement('link');
+$href = $dom->createAttribute('href');
+$href->value = $OB_LABEL_FONT_LINK;
+$link->appendChild($href);
+$rel = $dom->createAttribute('rel');
+$rel->value = 'stylesheet';
+$link->appendChild($rel);
+$type = $dom->createAttribute('type');
+$type->value = 'text/css';
+$link->appendChild($type);
+
+$dom->appendChild($link);
+
+
+$link = $dom->createElement('link');
+$href = $dom->createAttribute('href');
+$href->value = $SUBMIT_FONT_LINK;
+$link->appendChild($href);
+$rel = $dom->createAttribute('rel');
+$rel->value = 'stylesheet';
+$link->appendChild($rel);
+$type = $dom->createAttribute('type');
+$type->value = 'text/css';
+$link->appendChild($type);
+
+$dom->appendChild($link);
+
+
+$link = $dom->createElement('link');
+$href = $dom->createAttribute('href');
+$href->value = $OB_LABEL_FONT_LINK;
+$link->appendChild($href);
+$rel = $dom->createAttribute('rel');
+$rel->value = 'stylesheet';
+$link->appendChild($rel);
+$type = $dom->createAttribute('type');
+$type->value = 'text/css';
+$link->appendChild($type);
+
+$dom->appendChild($link);
+
+
+$link = $dom->createElement('link');
+$href = $dom->createAttribute('href');
+$href->value = $HEADER_FONT_LINK;
+$link->appendChild($href);
+$rel = $dom->createAttribute('rel');
+$rel->value = 'stylesheet';
+$link->appendChild($rel);
+$type = $dom->createAttribute('type');
+$type->value = 'text/css';
+$link->appendChild($type);
+
+$dom->appendChild($link);
+
+
+$link = $dom->createElement('link');
+$href = $dom->createAttribute('href');
+$href->value = $LABEL_FONT_LINK;
+$link->appendChild($href);
+$rel = $dom->createAttribute('rel');
+$rel->value = 'stylesheet';
+$link->appendChild($rel);
+$type = $dom->createAttribute('type');
+$type->value = 'text/css';
+$link->appendChild($type);
+
+$dom->appendChild($link);
+
+
+/*
+option texts are <label>
+form header is <h1>
+object labels are <h4>
+*/
+
+$css_text = file_get_contents($FORM_CSS);
+
+$style = $dom->createElement('style', $css_text);
+$dom->appendChild($style);
+
   $OUTPUT_FILE  = 'setup.cfg';
   $FORMAT_XML   = 'description.xml';
   $FORM_HANDLER = 'form_processor.php';
@@ -42,7 +134,7 @@ fclose($cfgfile);
   $FORM_HEADER = trim($xml->title);
   $filewrite = "";                              //the string to be written to the file
   $cfgfile = fopen($OUTPUT_FILE, "w");
-
+  $Jscript = file_get_contents($VALIDATOR_JS);
   $model_type = file_get_contents('umtype.txt');
   $umtype = trim($model_type);
   $filewrite .= 'UMType = '.$model_type.PHP_EOL;
@@ -52,12 +144,12 @@ fclose($cfgfile);
   //if($model_type == 'regional') $model = $xml->regional->objects;
   foreach($xml->children() as $thing){
     if($model_type == $thing->getName()){
-      $model = $thing->objects;
+      $model = $thing;
       break;
     }
   }
 
-  foreach($model->children() as $object){
+  foreach($model->objects->children() as $object){
     $ob_label = trim($object->label);             //object label
     $ob_type = trim($object->inputType);          //object type
     $ob_descript = trim($object->description);
@@ -122,7 +214,114 @@ fclose($cfgfile);
     $filewrite = "";
 
   }//end of foreach object------------------------------------------------------
-  echo "SUCCESS: File has been created";
+  echo "<h3>SUCCESS: File has been created</h3>";
   fclose($cfgfile);
+  ///
+  ///
+  ///
 
+  ///
+  $FORM_HANDLER = "vars_processor.php";
+  //echo $model->varsfile;
+  $varsxml = trim($model->varsfile);
+  $xml = simplexml_load_file($varsxml) or die("Error: FATAL ERROR. POSSIBLY CORRUPT XML FORMAT.");
+
+
+  $header = $dom->createElement('h4', 'setup.cfg generated, please select variables for vars.cfg file');
+  $dom->appendChild($header);
+  $form = $dom->createElement('form');
+  $attr = $dom->createAttribute('method');
+  $attr->value = 'post';
+  $form->appendChild($attr);
+  $attr = $dom->createAttribute('action');
+  $attr->value = $FORM_HANDLER;
+  $form->appendChild($attr);
+  $dom->appendChild($form);
+  $form->appendChild($dom->createElement('br')); // line breaker
+
+  foreach($xml->children() as $var){
+    $name = trim($var->name);
+    $stash = trim($var->stash);
+    $descript = trim($var->description);
+
+    $hidden_descript = $dom->createElement('input');
+    $attr = $dom->createAttribute('value');
+    $attr->value = $descript;
+    $hidden_descript->appendChild($attr);
+    $attr = $dom->createAttribute('type');
+    $attr->value = 'hidden';
+    $hidden_descript->appendChild($attr);
+    $attr = $dom->createAttribute('id');
+    $attr->value = $name.'  :  '.$stash;
+    $hidden_descript->appendChild($attr);
+    $dom->appendChild($hidden_descript);
+
+
+    $input = $dom->createElement('input');
+    $attr = $dom->createAttribute('name');
+    $attr->value = 'checklist[]';
+    $input->appendChild($attr);
+    $attr = $dom->createAttribute('type');
+    $attr->value = 'checkbox';
+    $input->appendChild($attr);
+    $attr = $dom->createAttribute('value');
+    $attr->value = '('.$name.', '.$stash.')';
+    $input->appendChild($attr);
+    $attr = $dom->createAttribute('checked');
+    $input->appendChild($attr);
+    $form->appendChild($input);
+    $label = $dom->createElement('label', $name.'  :  '.$stash);
+    $attr = $dom->createAttribute('onmouseover');
+    $attr->value = 'descriptor(this)';    // and this.value is the ob_label which inturn is the ID of it's descript
+    $label->appendChild($attr);
+    $form->appendChild($label);
+    $form->appendChild($dom->createElement('br')); // line breaker
+    $form->appendChild($dom->createElement('br')); // line breaker
+  }
+  $form->appendChild($dom->createElement('br')); // line breaker
+  $input = $dom->createElement('input');
+  $attr = $dom->createAttribute('type');
+  $attr->value = 'submit';
+  $input->appendChild($attr);
+  $attr = $dom->createAttribute('value');
+  $attr->value = 'SUBMIT';
+  $input->appendChild($attr);
+  $attr = $dom->createAttribute('style');
+  $attr->value = "font-family: 'Oswald', sans-serif; position: relative; left: 37.9%; font-size: 100%;";
+  $input->appendChild($attr);
+  $form->appendChild($input);
+  $form->appendChild($dom->createElement('br')); // line breaker
+  $form->appendChild($dom->createElement('br')); // line breaker
+
+
+  //=========================================================================//
+
+    //status div for description and instructions
+    $div2 = $dom->createElement('div');
+    $attr = $dom->createAttribute('class');
+    $attr->value = 'description';
+    $div2->appendChild($attr);
+
+    $stray_header = $dom->createElement('h4', 'HOVER MOUSE OVER FIELD NAME FOR DESCRIPTION');
+    $div2->appendChild($stray_header);
+    //=================DESCRIPTION CONTENT HERE ====================//
+    //$div2->appendChild($dom->createElement('br')); // div2 line break
+    $content = $dom->createElement('p', '');
+    $attr = $dom->createAttribute('id');
+    $attr->value = "descript";
+    $content->appendChild($attr);
+    //$attr = $dom->createAttribute('class');
+    $div2->appendChild($content);
+
+    $dom->appendChild($div2);
+
+
+    $dom->appendChild($dom->createElement('br')); // dom line breaker
+    $dom->appendChild($dom->createElement('br')); // dom line breaker
+
+
+    $script = $dom->createElement('script', $Jscript);
+    $dom->appendChild($script);
+
+  echo $dom->saveHTML();
 ?>
