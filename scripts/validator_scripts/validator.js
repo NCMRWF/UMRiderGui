9f9fa5e3cda4b14c694d67879b1373d2dd5ff94d
arditecht <rickdasgupta9@gmail.com>
2016-06-20 13:01:19 +0530

    more dynamic

--- scripts/validator_scripts/validator.js
+++ scripts/validator_scripts/validator.js
@@ -1,11 +1,22 @@
 function descriptor(obj){
   var getter = obj.innerHTML;
-  var ID = (getter.slice(0, -2)).trim();
+
+  var ID = (getter);
 
   var ele = document.getElementById(ID);
   document.getElementById("descript").innerHTML = ele.value;
 }
+/*
+function descriptor_vars(obj){
+  var getter = obj.innerHTML;
+  var str_arr = getter.split(" ");
+  getter = str_arr[0]
+  var ID = (getter).trim();
 
+  var ele = document.getElementById(ID);
+  document.getElementById("descript").innerHTML = ele.value;
+}
+*/
 function clear_descript(){
-  document.getElementById("descript").innerHTML = '';
+  //document.getElementById("descript").innerHTML = '';
 }
